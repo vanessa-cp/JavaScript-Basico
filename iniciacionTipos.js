c58be00713822265df53bf58d8a54fe3b4073fb3
vanessa.cp <vanehom@hotmail.com>
2023-01-11 19:55:52 +0100

    Tipos.js

---
+++ iniciacionTipos.js
@@ -0,0 +1,17 @@
+//Tipos primitivos
+//Number
+4
+
+//string
+"Hola mundo";
+'Hola mundo';
+`Hola mundo`;
+
+//Booleanos
+true;
+false;
+
+//Nulo Undefined
+null;//espacio vacio en memoria
+undefined;//valor indefinido(no esta asignado en memoria)
+//null, undefined,false,0
